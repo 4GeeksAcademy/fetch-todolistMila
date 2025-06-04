0297120e33d8f60f47d379357e0967d45d3cf60f
Milangela-wpz <milangela.1920@gmail.com>
2025-06-04 19:54:41 +0000

    First commit

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,100 @@
-import React from "react";
+import React, { useEffect, useState }from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	const [task, setTask] = useState ('');
+	const [list, setList] = useState ([]);
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+	const handleChange = (e) => {
+		setTask(e.target.value)
+	};
+
+	const handleKeyDown = (e) => {
+		if (e.key ==='Enter') {
+			e.preventDefault();
+			agregarTarea();
+		}
+	};
+
+	const agregarTarea = async () => {
+		if (task.trim() === '') return;
+		await fetch ('https://playground.4geeks.com/todo/todos/Milangelawpz', {
+			method: 'POST',
+			body: JSON.stringify(
+				{
+					'label': task,
+					'is_done': false
+				}
+			),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+		setTask('');
+		await getTareasMilangelawpz();
+	};
+
+	const getTareasMilangelawpz = async () => {
+		const response = await fetch('https://playground.4geeks.com/todo/users/Milangelawpz')
+		if (!response.ok) {
+			await crearMilangelawpz();
+		}
+		const data= await response.json();
+		setList(data.todos)	
+	};
+
+	const crearMilangelawpz = async () => {
+		await fetch('https://playground.4geeks.com/todo/users/Milangelawpz', {method: 'POST'});
+	};
+
+	useEffect(() => {
+		getTareasMilangelawpz()
+	}, []);
+
+	const handleDelete = async (id) => {
+		await fetch(`https://playground.4geeks.com/todo/todos/${id}`, {
+			method: 'DELETE'
+		});
+		await getTareasMilangelawpz();
+	};
+
+	return (
+    <div className="todoContainer">
+      <h1>To Do List</h1>
+      <input
+        type="text"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+        onKeyDown={handleKeyDown}
+        placeholder="Anything  else?"
+      />
+
+      <div>
+        {list.length === 0 ? (
+          <div className="noTask">All done, time to chill 👽</div>
+        ) : (
+          list.map((t) => (
+            <div key={t.id} className="taskItem">●
+              {t.label}
+              <span
+                className="deleteBtn"
+                onClick={() => handleDelete(t.id)}
+              >
+                x
+              </span>
+            </div>
+          ))
+        )}
+      </div>
+
+      <div className="footer">{list.length} task(s)</div>
+    </div>
+  );
+
 };
+
 
 export default Home;
